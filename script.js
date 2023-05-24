afbbf090362ba9137dde6311e87f5326f3fe71da
Felipe Urriago <durriago82@gmail.com>
2023-05-23 19:18:14 -0500

    organicé la información y reestructuré el código para que quede más interesante

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
-// se crea el array que contiene los elementos de la lista de contactos
-let arrayContactos = ["David Urriago", "Juan Almario", "Daniel Piedrahita"];
+// cree el array que contiene los elementos de la lista de contactos
+    let miArrayContactos = ["David Urriago", "Juan Almario", "Daniel Piedrahita"]; 
 
 
-// se define la funcion para imprimir los elementos del array, se agrega el bucle for para que recorra todo el array y que despues imprima los cambios en el array
-function imprimirContactos() {
+// definí la función para imprimir los elementos del array, se agrega el bucle for para que recorra todo el array y que después imprima los cambios en el array
+ function mostrarContactos() {
   console.log("Lista de contactos:");
 
- for (let i = 0; i < arrayContactos.length; i++) {
-  console.log(arrayContactos[i]);
+    for (let i = 0; i <   miArrayContactos.length; i++) {
+    console.log(miArrayContactos[i]);
 
 
 
   }
-}
+} 
 
 
-// se define la funcion agregarContactos para hacer un Push a la lista y de esta manera agregar elementos al array
-function agregarContacto(nombre) {
-  arrayContactos.push(nombre);
+// aaqui definí la función para agregarContactos, lo que hace es agregar al final de array un nuevo elemento
 
-}
-
-// esta función quita un contacto existente del array, declaró la variable arrayContactosNuevos el método filter este va a crear un nuevo array filtrando todos los datos distintos a los que se le dio en nombre al principio de la variable 
-function borrarContacto(nombre) {
-  const arrayContactosNuevos = arrayContactos.filter(contacto => contacto !== nombre);
-  const quitarContacto = arrayContactosNuevos.length < arrayContactos.length;
-  
-  if (quitarContacto) {
-    arrayContactos = arrayContactosNuevos;
+     function agregarContacto(nombre) {
+      console.log("Contacto agregado");
+        miArrayContactos.push(nombre);
 
 
+} 
 
-  }
-}
+// esta es la función para borrar un contacto, lo que hace la función es buscar en el array el elemento que le di en nombre, si el index existe se sitúa en un elemento que no existe, muestra la cadena de texto: el contacto no existe, de lo contrario si el index existe lo borra con splice y muestra el texto: borrado con éxito.
+
+    function borrarContacto (nombre) {
+        let index = miArrayContactos.indexOf(nombre);
+         if (index === -1)  { 
+          console.log("Contacto no existe");}
+          else { 
+            console.log("Borrado con exito")
+            miArrayContactos.splice (index, 1);}
+         
+       
+
+
+
+
+    }
+
+  //Método de uso:
+      //para mostrar el array se le da la instrucción mostrarContactos();
+      //para agregar se le da la instrucción agregarContacto("Nombre Apellido") para añadir un nuevo contacto
+      //para eliminar se le da borrarContacto("Nombre Apellido") se debe escribir el nombre tal cual, de lo contrario, devuelve el texto: Contacto no existe
+    
+
+
+
+
+
 
